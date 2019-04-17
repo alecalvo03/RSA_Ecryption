ec77eaa2fbad6c44f2914fb9314a2ab33d9dccd1
Alejandro <ale_calvo03@outlook.com>
2019-04-16 21:38:35 -0600

    mul update, div progress

--- RSA.asm
+++ RSA.asm
@@ -1,21 +1,21 @@
 %include "io64.inc"
 
 section	.data
-    bignum1 dq 4,0,0,0;0xFFFFFFFFFFAFFFFF, 0xFFFFFBFFFFFFFFFF, 0, 0
-    bignum2 dq 6,3,0,0;0xFFFFFFFFFFFFFFCF, 0xFFFDFFFFFFFFFFFF, 0, 0
-    mul1    dq 0, 0
-    mul2    dq 0, 0
-    mul3    dq 0, 0
-    mul4    dq 0, 0
+    bignum1 dq 2, 2, 0, 0;0xFFFFFFFFFFAFFFFF, 0xFFFFFBFFFFFFFFFF, 0, 0
+    bignum2 dq 4, 6, 0, 0;0xFFFFFFFFFFFFFFCF, 0xFFFDFFFFFFFFFFFF, 0, 0
+    mul1    dq 0xFFFFFFFFFFAFFFFF, 0xFFFFFBFFFFFFFFFF, 0xFFF3FFFFFFFFFFFF, 0xFFFFF1FFFFFFFFFF
+    mul2    dq 0xFFFFFF4FFFFFFFFF, 0xFFFFFFFFFF6FFFFF, 0xFFFFFFFFFF2FFFFF, 0xFFF7FFFFFFFFFFFF
+    mulr    dq 0, 0, 0, 0
 
     divD    dq 0, 0, 0, 0
     divN    dq 0, 0, 0, 0
     divQ    dq 0, 0, 0, 0
-    divR    dq 0, 0, 0, 0
+    divQc   dq 0, 0, 0, 0
     divM    dq 0
-    divA    dq 0, 0, 0, 0
+    divNo   dq 0, 0, 0, 0
     
     
+
 
 section .text
 	global main
@@ -25,10 +25,17 @@ main:
 
     call bigdiv
     
-    mov rax, qword[bignum2]
-    mov rbx, qword[bignum2+8]
-    mov rcx, qword[bignum2+16]
-    mov rdx, qword[bignum2+24]
+    mov rax, qword[mulr]
+    mov rbx, qword[mulr+8]
+    mov rcx, qword[mulr+16]
+    mov rdx, qword[mulr+24]
+    
+    PRINT_HEX 8, bignum2+24
+    PRINT_HEX 8, bignum2+16
+    PRINT_HEX 8, bignum2+8
+    PRINT_HEX 8, bignum2
+    NEWLINE
+
 
 bigadd:                                 ;bignum2 = bignum2 + bignum1
     mov rax, qword [bignum1]
@@ -52,77 +59,84 @@ bigsub:                                 ;bignum2 = bignum2 - bignum1
     sbb qword [bignum2+24], rax
     ret
 
-bigmul:                                  ;bignum2 = bignum2 * bignum1
+bigmul:
+    mov qword[mulr], 0
+    mov qword[mulr+8], 0
+    mov qword[mulr+16], 0
+    mov qword[mulr+24], 0
 
-    mov rax, qword [bignum1]		; move bignum1[0] to rax
-    mul qword [bignum2]				; rdx:rax = bignum1[0]*bignum2[0]
-    mov qword [mul1], rax			; move rax to mul1[0]
-    mov qword [mul1+8], rdx			; move rdx to mul1[1]
-    mov rax, qword [bignum1]		; move bignum1[0] to rax
-    mul qword [bignum2+8]			; rdx:rax = bignum1[0]*bignum2[1]
-    mov qword [mul2], rax			; move rax to mul2[0]
-    mov qword [mul2+8], rdx			; move rdx to mul2[1]
-    mov rax, qword [bignum1+8]		; move bignum1[1] to rax
-    mul qword [bignum2]				; rdx:rax = bignum1[1]*bignum2[0]
-    mov qword [mul3], rax			; move rax to mul3[0]
-    mov qword [mul3+8], rdx			; move rdx to mul3[1]
-    mov rax, qword [bignum1+8]		; move bignum1[1] to rax
-    mul qword [bignum2+8]			; rdx:rax = bignum1[1]*bignum2[1]
-    mov qword [mul4], rax			; move rax to mul4[0]
-    mov qword [mul4+8], rdx			; move rdx to mul4[1]
-	;mov rax, qword[mul1]			; move mul1[0] to rax
-	;mov qword[bignum2], rax			; move rax to bignum2[0]
+    mov rax, qword[bignum1]		                 ; move mul1[0] to rax
+    mul qword[bignum2]				; rdx:rax = mlu1[0]*mul2[0]
+    
+    mov qword[mulr], rax			        ; move rax to mul3[0]
+    mov qword[mulr+8], rdx		        ; move rdx to mul3[1]
+    
+    mov rax, qword [bignum1]		        ; move mul1[0] to rax
+    mul qword [bignum2+8]		                ; rdx:rax = mul1[0]*mul2[1]
+    
+    add qword[mulr+8], rax
+    adc qword[mulr+16], rdx
+    adc qword[mulr+24], 0
+    
+    mov rax, qword [bignum1]		        ; move mul1[0] to rax
+    mul qword [bignum2+16]				; rdx:rax = mul1[0]*mul2[2]
+    
+    add qword[mulr+16], rax
+    adc qword[mulr+24], rdx
+    
+    mov rax, qword [bignum1]		        ; move mul1[0] to rax
+    mul qword [bignum2+24]			        ; rdx:rax = mul1[0]*mul2[3]
+    
+    add qword[mulr+24], rax
+    
 
-    mov rax, qword[mul1+8]
-    mov qword[bignum1],rax
-    mov qword[bignum1+8], 0
-    mov rax, qword[mul2]
+    mov rax, qword [bignum1+8]		        ; move mul1[1] to rax
+    mul qword [bignum2]		                ; rdx:rax = mul1[1]*mul2[0]
+    
+    add qword[mulr+8], rax
+    adc qword[mulr+16], rdx
+    adc qword[mulr+24], 0
+    
+    mov rax, qword [bignum1+8]		        ; move mul1[1] to rax
+    mul qword [bignum2+8]				; rdx:rax = mul1[1]*mul2[1]
+    
+    add qword[mulr+16], rax
+    adc qword[mulr+24], rdx
+    
+    mov rax, qword [bignum1+8]		        ; move mul1[1] to rax
+    mul qword [bignum2+16]			        ; rdx:rax = mul1[1]*mul2[2]
+    
+    add qword[mulr+24], rax
+    
+
+    mov rax, qword [bignum1+16]		        ; move mul1[2] to rax
+    mul qword [bignum2]				; rdx:rax = mul1[2]*mul2[0]
+    
+    add qword[mulr+16], rax
+    adc qword[mulr+24], rdx
+    
+    mov rax, qword [bignum1+16]		        ; move mul1[2] to rax
+    mul qword [bignum2+8]			        ; rdx:rax = mul1[2]*mul2[1]
+    
+    add qword[mulr+24], rax
+    
+
+    mov rax, qword [bignum1+24]		        ; move mul1[3] to rax
+    mul qword [bignum2]			        ; rdx:rax = mul1[3]*mul2[0]
+    
+    add qword[mulr+24], rax
+    
+    
+    mov rax, qword[mulr]
     mov qword[bignum2], rax
-    mov qword[bignum2+8], 0
-    call bigadd
-    mov rax, qword[mul3]
-    mov qword[bignum1], rax
-    call bigadd
-
-    mov rdx, qword[bignum2]
-    mov qword[mul2], rdx
-    mov rdx, qword[bignum2+8]
-
-    mov rax, qword[mul2+8]
-    mov qword[bignum1],rax
-    mov qword[bignum1+8], 0
-    mov rax, qword[mul3+8]
-    mov qword[bignum2], rax
-    mov qword[bignum2+8], 0
-    call bigadd
-    mov rax, qword[mul4]
-    mov qword[bignum1], rax
-    mov qword[bignum1+8], 0
-    call bigadd
-    mov qword[bignum1], rdx			; add remainder
-    mov qword[bignum1+8], 0
-    call bigadd
-
-    mov rdx, qword[bignum2]
-    mov qword[mul3], rdx
-    mov rdx, qword[bignum2+8]
-
-    mov rax, qword[mul4+8]
-    add rax, rdx
-
-    mov qword[bignum2+24], rax
-    mov rax, qword[mul3]
-    mov qword[bignum2+16], rax
-    mov rax, qword[mul2]
+    mov rax, qword[mulr+8]
     mov qword[bignum2+8], rax
-    mov rax, qword[mul1]
-    mov qword[bignum2], rax
-
+    mov rax, qword[mulr+16]
+    mov qword[bignum2+16], rax
+    mov rax, qword[mulr+24]
+    mov qword[bignum2+24], rax
+    
     ret
-    
-bigmul2:
-    
-    
 
 getmagnitude:
     mov rax, 24
@@ -154,22 +168,21 @@ bigdiv:                                         ;bignum2 = bignum2 / bignum1
     
     mov rax, qword[bignum2]
     mov qword[divN], rax
+    mov qword[divNo], rax
     mov rax, qword[bignum2+8]
     mov qword[divN+8], rax
+    mov qword[divNo+8], rax
     mov rax, qword[bignum2+16]
     mov qword[divN+16], rax
+    mov qword[divNo+16], rax
     mov rax, qword[bignum2+24]
     mov qword[divN+24], rax
+    mov qword[divNo+24], rax
     
-    mov qword[divA], 0
-    mov qword[divA+8], 0
-    mov qword[divA+16], 0
-    mov qword[divA+24], 0
-    
-    mov qword[divR], 0
-    mov qword[divR+8], 0
-    mov qword[divR+16], 0
-    mov qword[divR+24], 0
+    mov qword[divQc], 0
+    mov qword[divQc+8], 0
+    mov qword[divQc+16], 0
+    mov qword[divQc+24], 0
     
     mov qword[divQ], 0
     mov qword[divQ+8], 0
@@ -177,6 +190,8 @@ bigdiv:                                         ;bignum2 = bignum2 / bignum1
     mov qword[divQ+24], 0
     
     call getmagnitude
+    
+divloop:
 
     mov rcx, qword[divM]
     
@@ -201,26 +216,19 @@ bigdiv:                                         ;bignum2 = bignum2 / bignum1
     jz divdone
     
     mov rax, qword[divN]
-    div qword[divA+rcx]
+    div qword[divD+rcx]
     mov qword[divQ], rax
-    
-    ;stores divD in bignum2
-    mov rax, qword[divD]
-    mov qword[bignum2], rax
-    mov rax, qword[divD+8]
-    mov qword[bignum2+8], rax
-    mov rax, qword[divD+16]
-    mov qword[bignum2+16], rax
-    mov rax, qword[divD+24]
-    mov qword[bignum2+24], rax
-    
-    mov qword[bignum1+24], 0
-    mov qword[bignum1+16], 0
-    mov qword[bignum1+8], 0
     
 divdone:
 
     mov rbx, 24 ;initial condition
+    cmp rcx, 0
+    jz shifteddivQ
+    
+    mov qword[bignum1+24], 0
+    mov qword[bignum1+16], 0
+    mov qword[bignum1+8], 0
+    mov qword[bignum1], 0
     
 shiftdivQ: ;Shift divQ and stores in bignum1
     mov rax, qword[divQ+rbx]
@@ -229,7 +237,6 @@ shiftdivQ: ;Shift divQ and stores in bignum1
     cmp rbx, rcx
     jge shiftdivQ
     
-    ;Stores divQ shifted
     mov rax, qword[bignum1]
     mov qword[divQ], rax
     mov rax, qword[bignum1+8]
@@ -239,25 +246,131 @@ shiftdivQ: ;Shift divQ and stores in bignum1
     mov rax, qword[bignum1+24]
     mov qword[divQ+24], rax
     
+shifteddivQ:
+    
+    ;Stores divQ shifted
+    mov rax, qword[divQ]
+    mov qword[bignum2], rax
+    mov rax, qword[divQ+8]
+    mov qword[bignum2+8], rax
+    mov rax, qword[divQ+16]
+    mov qword[bignum2+16], rax
+    mov rax, qword[divQ+24]
+    mov qword[bignum2+24], rax
+    
+    PRINT_HEX 8, divQ+24
+    PRINT_HEX 8, divQ+16
+    PRINT_HEX 8, divQ+8
+    PRINT_HEX 8, divQ
+    NEWLINE
+    
+    mov rax, qword[divQc]
+    mov qword[bignum1], rax
+    mov rax, qword[divQc+8]
+    mov qword[bignum1+8], rax
+    mov rax, qword[divQc+16]
+    mov qword[bignum1+16], rax
+    mov rax, qword[divQc+24]
+    mov qword[bignum1+24], rax
+    call bigadd
+    
+    PRINT_HEX 8, bignum2+24
+    PRINT_HEX 8, bignum2+16
+    PRINT_HEX 8, bignum2+8
+    PRINT_HEX 8, bignum2
+    NEWLINE
+    
+    mov rax, qword[bignum2]
+    mov qword[divQc], rax
+    mov rax, qword[bignum2+8]
+    mov qword[divQc+8], rax
+    mov rax, qword[bignum2+16]
+    mov qword[divQc+16], rax
+    mov rax, qword[bignum2+24]
+    mov qword[divQc+24], rax
+    
+    ;stores divD in bignum2
+    mov rax, qword[divD]
+    mov qword[bignum1], rax
+    mov rax, qword[divD+8]
+    mov qword[bignum1+8], rax
+    mov rax, qword[divD+16]
+    mov qword[bignum1+16], rax
+    mov rax, qword[divD+24]
+    mov qword[bignum1+24], rax
+    
     PRINT_HEX 8, bignum1+24
     PRINT_HEX 8, bignum1+16
     PRINT_HEX 8, bignum1+8
     PRINT_HEX 8, bignum1
-    
     NEWLINE
-    
-    PRINT_HEX 8, bignum2+24
-    PRINT_HEX 8, bignum2+16
-    PRINT_HEX 8, bignum2+8
-    PRINT_HEX 8, bignum2
     
     call bigmul
     
-    NEWLINE
     PRINT_HEX 8, bignum2+24
     PRINT_HEX 8, bignum2+16
     PRINT_HEX 8, bignum2+8
     PRINT_HEX 8, bignum2
+    NEWLINE
+    
+    mov rax, qword[bignum2]
+    mov qword[bignum1], rax
+    mov rax, qword[bignum2+8]
+    mov qword[bignum1+8], rax
+    mov rax, qword[bignum2+16]
+    mov qword[bignum1+16], rax
+    mov rax, qword[bignum2+24]
+    mov qword[bignum1+24], rax
+    
+    mov rax, qword[divNo]
+    mov qword[bignum2], rax
+    mov rax, qword[divNo+8]
+    mov qword[bignum2+8], rax
+    mov rax, qword[divNo+16]
+    mov qword[bignum2+16], rax
+    mov rax, qword[divNo+24]
+    mov qword[bignum2+24], rax
+    call bigsub
+    
+    PRINT_HEX 8, bignum2+24
+    PRINT_HEX 8, bignum2+16
+    PRINT_HEX 8, bignum2+8
+    PRINT_HEX 8, bignum2
+    NEWLINE
+    
+    
+    mov rax, qword[bignum2]
+    mov qword[divN], rax
+    mov rax, qword[bignum2+8]
+    mov qword[divN+8], rax
+    mov rax, qword[bignum2+16]
+    mov qword[divN+16], rax
+    mov rax, qword[bignum2+24]
+    mov qword[divN+24], rax
+    
+    ;compares R and D
+    mov rax, qword[divD+24]
+    cmp qword[bignum2+24], rax
+    jg divloop
+    mov rax, qword[divD+16]
+    cmp qword[bignum2+16], rax
+    jg divloop
+    mov rax, qword[divD+8]
+    cmp qword[bignum2+8], rax
+    jg divloop
+    mov rax, qword[divD]
+    cmp qword[bignum2], rax
+    jge divloop
+    
+    
+    mov rax, qword[divQ]
+    mov qword[bignum2], rax
+    mov rax, qword[divQ+8]
+    mov qword[bignum2+8], rax
+    mov rax, qword[divQ+16]
+    mov qword[bignum2+16], rax
+    mov rax, qword[divQ+24]
+    mov qword[bignum2+24], rax
     
     ret
     
